a89329a73adf648f6e01e93f00e58c80e0e95f3b
Yuri Cicconi <yuricbianchii@gmail.com>
2023-04-06 14:25:42 -0300

    Alterações realizadas durante a primeira aula do Segundo Curso.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O Senhor dos Anéis - A Sociedade do Anel";
+        meuFilme.anoDeLancamento = 2001;
+        meuFilme.duracaoEmMinutos = 178;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(10);
+        meuFilme.avalia(8);
+        meuFilme.avalia(7);
+
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
     }
 }
